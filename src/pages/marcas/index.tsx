f43ce8fa793ecc4f74214fe9ec0332426266fc8c
fsvboas <felippesantosvb@hotmail.com>
2022-05-09 19:06:40 -0300

    Create Brands page and styles

--- src/pages/marcas/index.tsx
+++ src/pages/marcas/index.tsx
@@ -1,22 +1,38 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
+import Button from "../../components/Button";
 
 import { Header } from "../../components/Header";
 
-// import styles from "../../styles/pages/Home.module.css";
+import styles from "../../styles/pages/marcas/Brands.module.css";
 
 const Brands: NextPage = () => {
   return (
     <div>
       <Head>
-        <title>Gerenciamento de carros</title>
+        <title>Gerenciamento | Marcas</title>
         {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
 
       <Header />
 
-      <h1>PAGINA MARCAS</h1>
+      <div className={styles.pageContainer}>
+        <div className={styles.title}>
+          <h1>Marcas</h1>
+          <Link href="/marcas/novo">
+            <Button
+              text="Novo carro"
+              padding="0.5rem 2rem"
+              radius="0.25rem"
+              color="#fff"
+              margin="0"
+              onClick={() => console.log("Clicou novo carro pela aba marcas")}
+            />
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };

32aba549a8af6bdf6a4b6b9b8b26479e4f7dfade
fsvboas <felippesantosvb@hotmail.com>
2022-05-11 16:07:40 -0300

    Adjust table component and create delete method

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -2,71 +2,96 @@ import Link from "next/link";
 import { ICar } from "../pages/carros";
 import styles from "../styles/components/Table.module.css";
 import Button from "./Button";
-import { useEffect, useState } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import { mappedObjectDataKeyToPortuguese } from "../utils/mapObjectDataKeyToPortuguese";
+import { createConnection } from "../pages/api/createConnection";
+import { deleteCar } from "../pages/api/cars/deleteCar";
+import { getCars } from "../pages/api/cars/getCar";
+import { getBrands } from "../pages/api/brands/getBrand";
+import { deleteBrand } from "../pages/api/brands/deleteBrand";
 
-interface IProps {
-  data: ICar[];
+type headerProps = {
+  id: string;
+  label: string;
+  renderRow?: boolean;
+};
+interface TableProps {
+  data: ICar[] | { name: string }[];
+  header: headerProps[];
+  isObjectCar: boolean;
+  path: string;
 }
 
-export function Table(props: IProps) {
-  const [dataList, setDataList] = useState<{ key: string; value: any }[]>([]);
+export function Table(props: TableProps) {
+  const [data, setData] = useState<ICar[] | { name: string }[]>([]);
 
-  function dynamicTable(item: any) {
-    item.forEach((i: any) => {
-      const list = [];
-
-      for (const dynamicTableData in i) {
-        list.push({
-          key: mappedObjectDataKeyToPortuguese(dynamicTableData),
-          value: i[dynamicTableData],
+  async function deleteData(id: number) {
+    props.path.includes("carros")
+      ? await deleteCar(id).catch((error) => {
+          throw new Error(error);
+        })
+      : await deleteBrand(id).catch((error) => {
+          throw new Error(error);
         });
-      }
+  }
 
-      setDataList(list);
-    });
+  function getDataByPath() {
+    props.path.includes("carros")
+      ? getCars().then((res) => setData(res))
+      : getBrands().then((res) => setData(res));
   }
 
   useEffect(() => {
-    dynamicTable(props.data);
-  }, []);
+    getDataByPath();
+  }, [data.length]);
 
   return (
     <table className={styles.table}>
       <thead>
         <tr>
-          {dataList.map((d: any) => {
-            return <th>{d.key}</th>;
+          {props.header.map((d) => {
+            return <th>{d.label}</th>;
           })}
-          <th>Ações</th>
         </tr>
       </thead>
       <tbody>
-        <tr>
-          {dataList.map((d: any) => {
-            return <td>{d.value}</td>;
-          })}
-          <td>
-            <Link href="/carros/1">
-              <Button
-                text="Editar"
-                padding="0.25rem 1rem"
-                radius="0.25rem"
-                color="#fff"
-                margin="0 1rem 0 0"
-                onClick={() => console.log("Clicou em Editar")}
-              />
-            </Link>
-            <Button
-              text="Excluir"
-              padding="0.25rem 1rem"
-              radius="0.25rem"
-              color="#fff"
-              margin="0"
-              onClick={() => console.log("Clicou em Excluir")}
-            />
-          </td>
-        </tr>
+        {data.map((rowData: any) => {
+          return (
+            <tr>
+              {props.header.map((column) => {
+                if (column?.renderRow) {
+                  return (
+                    <td>
+                      <Link href={`${props.path}/${rowData.id}`}>
+                        <Button
+                          text="Editar"
+                          padding="0.25rem 1rem"
+                          radius="0.25rem"
+                          color="#fff"
+                          margin="0 1rem 0 0"
+                          onClick={() => console.log("Clicou em Editar")}
+                        />
+                      </Link>
+                      <Button
+                        text="Excluir"
+                        padding="0.25rem 1rem"
+                        radius="0.25rem"
+                        color="#fff"
+                        margin="0"
+                        onClick={() => {
+                          deleteData(rowData.id).then(() => {
+                            getDataByPath();
+                          });
+                        }}
+                      />
+                    </td>
+                  );
+                }
+                return <td>{rowData[column.id]}</td>;
+              })}
+            </tr>
+          );
+        })}
       </tbody>
     </table>
   );
